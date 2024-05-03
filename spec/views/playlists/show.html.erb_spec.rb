42388a4f2620a28b04aae007bf2e62b90f42773d
terandard <terandard0218@gmail.com>
2024-05-03 08:39:15 +0000

    Add `show.html.erb_spec.rb`

--- spec/views/playlists/show.html.erb_spec.rb
+++ spec/views/playlists/show.html.erb_spec.rb
@@ -3,5 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe 'playlists/show.html.erb' do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:playlist) { create(:playlist, identifier: 'identifier') }
+
+  before do
+    assign(:current_playlist, playlist)
+
+    track = create(:track, name: 'track_name')
+    create(:playlist_track, playlist:, track:)
+  end
+
+  it 'renders back link to playlist index' do
+    render
+
+    expect(rendered).to have_link('Back', href: playlists_path)
+  end
+
+  it 'renders duplicate link' do
+    render
+
+    expect(rendered).to have_link('Duplicate', href: playlist_duplicate_path('identifier'))
+  end
+
+  it 'renders sync link' do
+    render
+
+    expect(rendered).to have_link('Sync', href: sync_playlist_path('identifier'))
+  end
+
+  it 'renders track component' do
+    render
+
+    expect(rendered).to have_content('track_name')
+  end
 end
